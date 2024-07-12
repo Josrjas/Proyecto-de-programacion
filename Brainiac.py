3297bde6fcb71086a4421fdd33997d5e7a44fc16
Josrojas <josrojas2907@gmail.com>
2024-07-12 11:49:17 -0500

    Se agrego la funcionalidad del movimiento a la izquierda

--- Brainiac.py
+++ Brainiac.py
@@ -9,54 +9,64 @@ def valorar_movimientos(ficha, matriz_jugada, turno):
     x = int(ficha[0])
     y =int(ficha[1])
     
-    lista = {}
-    # Se asigna un puntaje a cada movimiento y se guarda en un diccionario hols
+    # Obtine el mejor valor
+    todos_los_valores = []
+    # Se asigna un puntaje a cada movimiento posible y se guarda en una lista
     if turno: # Juega con "X"
         if esta_en_rango(x - 2, y + 2) and puede_saltar(matriz_jugada, x - 1, y + 1, x - 2, y + 2):
-            lista[ficha] = 4
-        elif esta_en_rango(x - 2, y - 2) and puede_saltar(matriz_jugada, x - 1, y - 1, x - 2, y - 2):
-            lista[ficha] = -4
-        elif esta_en_rango(x - 1, y + 1) and (matriz_jugada[x - 1][y + 1] == "-"):
-            lista[ficha] = 3
-        elif esta_en_rango(x - 1, y - 1) and (matriz_jugada[x - 1][y - 1] == "-"):
-            lista[ficha] = -3
-        elif esta_en_rango(x + 1, y + 1) and (matriz_jugada[x + 1][y + 1] == "-"):
-            lista[ficha] = 2
-        elif esta_en_rango(x + 1, y - 1) and (matriz_jugada[x + 1][y - 1] == "-"):
-            lista[ficha] = -2
-        elif esta_en_rango(x + 2, y + 2) and puede_saltar(matriz_jugada, x + 1, y + 1, x + 2, y + 2):
-            lista[ficha] = 1
-        elif esta_en_rango(x + 2, y - 2) and puede_saltar(matriz_jugada, x + 1, y - 1, x + 2, y - 2):
-            lista[ficha] = -1
+            todos_los_valores.append(4)
+        if esta_en_rango(x - 2, y - 2) and puede_saltar(matriz_jugada, x - 1, y - 1, x - 2, y - 2):
+            todos_los_valores.append(-4)
+        if esta_en_rango(x - 1, y + 1) and (matriz_jugada[x - 1][y + 1] == "-"):
+            todos_los_valores.append(3)
+        if esta_en_rango(x - 1, y - 1) and (matriz_jugada[x - 1][y - 1] == "-"):
+            todos_los_valores.append(-3)
+        if esta_en_rango(x + 1, y + 1) and (matriz_jugada[x + 1][y + 1] == "-"):
+            todos_los_valores.append(2)
+        if esta_en_rango(x + 1, y - 1) and (matriz_jugada[x + 1][y - 1] == "-"):
+            todos_los_valores.append(-2)
+        if esta_en_rango(x + 2, y + 2) and puede_saltar(matriz_jugada, x + 1, y + 1, x + 2, y + 2):
+            todos_los_valores.append(1)
+        if esta_en_rango(x + 2, y - 2) and puede_saltar(matriz_jugada, x + 1, y - 1, x + 2, y - 2):
+            todos_los_valores.append(-1)
     else: # Juega con "O"
         if esta_en_rango(x + 2, y + 2) and puede_saltar(matriz_jugada, x + 1, y + 1, x + 2, y + 2):
-            lista[ficha] = 4
-        elif esta_en_rango(x + 2, y - 2) and puede_saltar(matriz_jugada, x + 1, y - 1, x + 2, y - 2):
-            lista[ficha] = -4
-        elif esta_en_rango(x + 1, y + 1) and (matriz_jugada[x + 1][y + 1] == "-"):
-            lista[ficha] = 3
-        elif esta_en_rango(x + 1, y - 1) and (matriz_jugada[x + 1][y - 1] == "-"):
-            lista[ficha] = -3
-        elif esta_en_rango(x - 1, y + 1) and (matriz_jugada[x - 1][y + 1] == "-"):
-            lista[ficha] = 2
-        elif esta_en_rango(x - 1, y - 1) and (matriz_jugada[x - 1][y - 1] == "-"):
-            lista[ficha] = -2
-        elif esta_en_rango(x - 2, y + 2) and puede_saltar(matriz_jugada, x - 1, y + 1, x - 2, y + 2):
-            lista[ficha] = 1
-        elif esta_en_rango(x - 2, y - 2) and puede_saltar(matriz_jugada, x - 1, y - 1, x - 2, y - 2):
-            lista[ficha] = -1
+            todos_los_valores.append(4)
+        if esta_en_rango(x + 2, y - 2) and puede_saltar(matriz_jugada, x + 1, y - 1, x + 2, y - 2):
+            todos_los_valores.append(-4)
+        if esta_en_rango(x + 1, y + 1) and (matriz_jugada[x + 1][y + 1] == "-"):
+            todos_los_valores.append(3)
+        if esta_en_rango(x + 1, y - 1) and (matriz_jugada[x + 1][y - 1] == "-"):
+            todos_los_valores.append(-3)
+        if esta_en_rango(x - 1, y + 1) and (matriz_jugada[x - 1][y + 1] == "-"):
+            todos_los_valores.append(2)
+        if esta_en_rango(x - 1, y - 1) and (matriz_jugada[x - 1][y - 1] == "-"):
+            todos_los_valores.append(-2)
+        if esta_en_rango(x - 2, y + 2) and puede_saltar(matriz_jugada, x - 1, y + 1, x - 2, y + 2):
+            todos_los_valores.append(1)
+        if esta_en_rango(x - 2, y - 2) and puede_saltar(matriz_jugada, x - 1, y - 1, x - 2, y - 2):
+            todos_los_valores.append(-1)
 
-    return lista
+    mayores_valores = [val for val in todos_los_valores if abs(val) == max(todos_los_valores)]
+    valor_escogido = random.choice(mayores_valores)
+
+    ficha_valor = {}
+    ficha_valor[ficha] = {valor_escogido}
+    
+
+    return ficha_valor
 
 def obtener_mejor_movimiento(turno, posicion_valor):
     letras = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H"}
+    #Encuentra el mayor valor de todas las fichas
     mayor_clave = max(posicion_valor.values(), key=abs)
-    maximo = [key for key, value in posicion_valor.items() if abs(value) == abs(mayor_clave)]
+    all_maximos = [key for key, value in posicion_valor.items() if abs(value) == abs(mayor_clave)]
+    
     jugada_posible = []
-
-    for posicion in maximo:
+    for posicion in all_maximos:
         cpu_jugada = ""
-        if turno:
+
+        if turno: # Convierte las posiciones a strings
             if posicion_valor[posicion] == 4:
                 cpu_jugada += f"{letras[int(posicion[1])]}{int(posicion[0])}{letras[int(posicion[1]) + 2]}{int(posicion[0]) - 2}"
             elif posicion_valor[posicion] == -4:
